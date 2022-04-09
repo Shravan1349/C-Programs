4ebb997948f03bc4756cd52ec0e8f99c7ee33700
Shravan1349 <101468124+Shravan1349@users.noreply.github.com>
2022-04-09 20:33:02 +0530

    Add files via upload

---
+++ .C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int i = 1, cnt = 1;
+
+    printf("/n Enter a number to print a table  :  ");
+    scanf("%d",&i);
+
+    for(cnt = 1; cnt<=10 ; cnt ++)
+    {
+
+        printf("\n %d",(i * cnt) );
+
+    }
+    getch();
+    return 0;
+
+
+}
