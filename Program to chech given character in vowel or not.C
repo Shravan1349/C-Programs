df52b3af5369904f393bdeede8440074692dd47d
Shravan1349 <101468124+Shravan1349@users.noreply.github.com>
2022-04-18 18:49:00 +0530

    Update Program to chech given character in vowel or not.C

--- Program to chech given character in vowel or not.C
+++ Program to chech given character in vowel or not.C
@@ -1,4 +1,4 @@
-
+///Program to check given letter is vowe;l or not.
 
 #include<stdio.h>
 #include<conio.h>
@@ -6,15 +6,32 @@
 int main()
 {
    char ch ='/0';
+   
+   printf("\n Enter A Character To Check Is  VOWEL or Not : ");
+   ch = getche();
 
-   printf("\n Enter A Character : ");
-   scanf("%c",&ch);
-
-   switch ch
-
-
-
-
-}
+   switch (ch) 
+   {    
+         
+      case 'A':
+      case 'a':
+      case 'E':
+      case 'e':
+      case 'I':
+      case 'i':
+      case 'O':
+      case 'o':
+      case 'U':
+      case 'u':  
+                printf("\n Entered Letter Is VOWEL.");
+                break;
+      default:
+              printf("\n Entered Letter is Not VOWEL.");
+         
+         printf("\n\n Thanks For Input.");
+         
+         getch();
+         return 0;                       
+   }
 
 
